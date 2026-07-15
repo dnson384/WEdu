94d30fbff7b943a409a90ae49703f2c5ad484445
dnson384 <doannhuson2004@gmail.com>
2026-07-15 17:09:11 +0700

    Revert "fix(server): fix missing refresh token in getMe flow"
    This reverts commit 3598e396eb165173bacaefdf1cb483b55df6eefa.

--- backend/src/main/java/com/fckedu/exam_creation/user/usecase/UserUsecase.java
+++ backend/src/main/java/com/fckedu/exam_creation/user/usecase/UserUsecase.java
@@ -156,6 +156,8 @@ public class UserUsecase {
             return refreshTokenService.delete(payload.getJti());
         } catch (JwtException | IllegalArgumentException ex) {
             throw new UnAuthorizedException("RT không hợp lệ");
+        } catch (InternalServerException ex) {
+            throw ex;
         }
     }
 
