856ea976305bc4c27883aa5dfea83ed63eee9bcf
dnson384 <doannhuson2004@gmail.com>
2026-08-04 17:22:04 +0700

    update(ui): generate - revise chapter selection page
    - Replace the chapter selection interface with an input field
    - Display selected chapters in a list

--- frontend/app/(site)/generate/[draftId]/page.tsx
+++ frontend/app/(site)/generate/[draftId]/page.tsx
@@ -119,7 +119,7 @@ export default function GenerateExam() {
                   >
                     Tiếp tục
                   </button>
-                </div>{" "}
+                </div>
               </div>
             </section>
           </main>
